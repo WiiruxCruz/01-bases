a930ea5f6bd1c2534e2a48fd7d35e8cb3febb461
WiiruxCruz <hialf.92@gmail.com>
2021-11-19 19:12:07 -0600

    Uso de ejemplo de two way databinding

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,19 +11,50 @@ interface Personaje{
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
-
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 9000
+    },{
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ]
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
 
   //agregar( event: any ) {
   agregar() {
     //event.preventDefault();
+
+    if( this.nuevo.nombre.trim().length === 0 ){
+      return ;
+    }
+
     console.log( this.nuevo );
+
+    this.personajes.push(this.nuevo);
+
+    //limpiar
+    /*
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+    */
+    this.limpiarDatos(); //por qué no funciona?
   }
 
   cambiarNombre( event:any ){
     console.log(event);
+  }
+
+  limpiarDatos():void {
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
   }
 }
