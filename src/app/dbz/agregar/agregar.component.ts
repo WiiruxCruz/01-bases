f5f6db4678c8ff8c3363a769299ce416828e65b3
WiiruxCruz <hialf.92@gmail.com>
2021-11-22 14:32:13 -0600

    Cambios al componente agregar para impl servicio

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -1,12 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html'
 })
 export class AgregarComponent {
+  //se agrega referencia al servicio
+  constructor( private DbzService: DbzService ) {}
 
   @Input()
   nuevo: Personaje = {
@@ -14,18 +17,17 @@ export class AgregarComponent {
     poder: 0
   }
 
-  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter<Personaje>();
+  //Se reemplaza emisión del evento por uso del servicio
+  //@Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter<Personaje>();
 
   //agregar( event: any ) {
   agregar() {
     //event.preventDefault();
 
-    if( this.nuevo.nombre.trim().length === 0 ){
-      return ;
-    }
+    if( this.nuevo.nombre.trim().length === 0 ){ return ; }
 
-    console.log( this.nuevo );
-    this.onNuevoPersonaje.emit( this.nuevo );
+    //this.onNuevoPersonaje.emit( this.nuevo );
+    this.DbzService.agregarPersonaje(this.nuevo);
 
 
     //limpiar
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -24,7 +24,9 @@ export class DbzService {
     return [...this._personajes] ;
   }
 
-  constructor(){
-    console.log('Servicio inicializado');
+  constructor(){}
+
+  agregarPersonaje( personaje: Personaje){
+    this._personajes.push(personaje);
   }
 }
